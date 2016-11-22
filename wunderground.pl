6b7e7f631cf744a8a0c990508f40e077947d7f46
Sjors Gielen <sjors@sjorsgielen.nl>
2016-11-22 20:48:49 +0100

    Use different centigrade character

--- wunderground.pl
+++ wunderground.pl
@@ -56,7 +56,7 @@ $dazeus->subscribe_command("weather" => sub {
 	}
 	$weather = $weather->[0];
 	# Nijmegen: Overcast, 11ºC, wind 5 km/h SSE, 91% humidity, sunset 4:36 PM CET
-	reply(sprintf("%s: %s, %.1f℃, wind %.1f km/h %s, %02d%% humidity, sunset %s", $location,
+	reply(sprintf("%s: %s, %.1f ºC, wind %.1f km/h %s, %02d%% humidity, sunset %s", $location,
 		map { $weather->{$_} } qw/conditions celsius wind_kilometersperhour wind_direction humidity sunset/),
 		$network, $sender, $channel);
 });
